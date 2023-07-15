dbf6ed7615c7d6a4172635011d76421f70172193
Singh <sabhisheksignh343104@gmail.com>
2023-07-15 21:06:47 +0530

    music.py file updated

--- music.py
+++ music.py
@@ -4,22 +4,19 @@ from playsound import playsound
 from random import choice
 from speak import speak
 from path import path
+from os import listdir
 
-songs = [
-	"Love Me Like You Do_320.mp3",
-	"I Am So Obsessed_320.mp3", 
-	"brokenAngel.mp3",
-]
+songs_list = listdir(path+"mp3/songs/")
 
 def mL(r):
 	if r == '':
-		playsound(path+"mp3/"+choice(songs))
+		playsound(path+"mp3/songs/"+choice(songs_list))
 	elif 'romantic ' in r:
-		playsound(f"{path}Love Me Like You Do_320(PagalWorld.com.se).mp3")
+		playsound(f"{path}mp3/songs/Love Me Like You Do_320(PagalWorld.com.se).mp3")
 	elif 'sad ' in r:
-		playsound(f"{path}I Am So Obsessed_320(PagalWorld).mp3")
+		playsound(f"{path}mp3/songs/I Am So Obsessed_320(PagalWorld).mp3")
 	elif 'broken ' in r:
-		playsound(f"{path}brokenAngel.mp3")
+		playsound(f"{path}mp3/songs/brokenAngel.mp3")
 	else:
 		try:
 			speak("Playing the song on Spotify")
@@ -30,8 +27,8 @@ def mL(r):
 
 def song():
 	print(f"{Fore.GREEN}\nThese three songs are default songs")
-	for i in range(len(songs)):
-		print(f"[{i+1}] ",f"{Fore.YELLOW}"+songs[i])
+	for i in range(len(songs_list)):
+		print(f"[{i+1}] ",f"{Fore.YELLOW}"+songs_list[i])
 	print(f'{Fore.CYAN}\nYou can play songs on Spotify and play some downloaded songs. Just say "Play [SONG NAME]".\n')
 	speak("You can play songs on Spotify and play some default songs. Just say Play SONG NAME", "hello.mp3")
 if __name__ == "__main__":
